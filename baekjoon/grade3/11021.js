753abbc68d423b3ad93fd4aa28a2f2860115dbf2
Kimbangul <highcolor_12@naver.com>
2023-08-01 13:40:09 +0900

    230801 A + B - 8

---
+++ baekjoon/grade3/11021.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+
+for (let i = 1; i <= parseInt(input[0]); i++) {
+  const num = input[i].split(' ').map((el) => parseInt(el));
+  console.log(`Case #${i}: ${num[0] + num[[1]]}`);
+}

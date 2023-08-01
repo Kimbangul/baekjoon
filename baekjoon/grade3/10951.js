0acf2485f1a6d95d5b1435cf3c57e112484a5df2
Kimbangul <highcolor_12@naver.com>
2023-08-01 14:15:43 +0900

    230801 A+B - 4

---
+++ baekjoon/grade3/10951.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+const numArr = input.map((el) => el.split(' ').map((num) => parseInt(num)));
+
+for (let i = 0; i < input.length; i++) {
+  console.log(numArr[i][0] + numArr[i][1]);
+}

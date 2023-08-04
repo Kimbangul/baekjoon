e9c8729ae4dfa14c62410a37624aee00a02c61e7
Kimbangul <highcolor_12@naver.com>
2023-08-04 15:17:23 +0900

    230804 새싹

---
+++ baekjoon/grade6/25083.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const input = fs.readFileSync('./input.txt').toString();
+const text = `         ,r'"7
+r\`\-_   ,\'  ,/
+ \\. \". L_r\'
+   \`\~\\/
+      |
+      |`;
+
+console.log(text);

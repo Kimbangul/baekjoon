d9a562f354d506cbc49b13f1492501631e9ec75e
Kimbangul <highcolor_12@naver.com>
2023-08-04 14:45:27 +0900

    230804 그대로 출력하기

---
+++ baekjoon/grade5/11718.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const input = fs.readFileSync('./input.txt').toString().trim();
+
+console.log(input);

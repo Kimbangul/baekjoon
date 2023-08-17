52b71a99e33018dab3275af26bf4b900b5f401cf
Kimbangul <highcolor_12@naver.com>
2023-08-18 08:12:32 +0900

    230818

---
+++ baekjoon/grade10/14215.js
@@ -0,0 +1,21 @@
+///dev/stdin
+const fs = require('fs');
+const input = fs
+  .readFileSync('./input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((el) => parseInt(el));
+
+const sortedSide = input.sort((a, b) => b - a);
+
+const otherSideLength = sortedSide[1] + sortedSide[2];
+
+if (otherSideLength > input[0]) {
+  console.log(input[0] + input[1] + input[2]);
+  return;
+}
+
+// else if ()
+
+console.log(sortedSide);

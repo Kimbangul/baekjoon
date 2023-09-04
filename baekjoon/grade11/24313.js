552b8e21f2b3b1c6f84afdb5b33e3dcea34f8350
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-09-04 23:23:59 +0900

    230904

--- baekjoon/grade11/24313.js
+++ baekjoon/grade11/24313.js
@@ -9,13 +9,7 @@ const a = input[0]
   .reverse();
 const c = parseInt(input[1]);
 const n0 = parseInt(input[2]);
-console.log(`${a[1]}n + ${a[0]} ≤ ${c} n`);
-console.log(`${a[0] * -1} / ${a[1] - c}`);
-const right = (a[0] * -1) / (a[1] - c);
-console.log(right);
-console.log(n0 <= right);
-if (a[1] - c >= 0) {
-  n0 <= right ? console.log(1) : console.log(0);
-} else {
-  n0 >= right ? console.log(1) : console.log(0);
-}
+console.log(`(${c}- ${a[1]})n >= ${a[0]} `);
+// 1. -100 <= a[0] <= 100, -100 <= a[1] <= 100
+// 2. -100 <= a[0] <= (c - a[1])n
+// 3. 0 <= a[0] + 100 <= (c - a[1])n + 100

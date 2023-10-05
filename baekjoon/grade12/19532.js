d93c7b719a83049c4385df1c155ad1314204e240
Kimbangul <highcolor_12@naver.com>
2023-10-06 08:56:26 +0900

    231006 수학은 비대면강의입니다

--- baekjoon/grade12/19532.js
+++ baekjoon/grade12/19532.js
@@ -7,7 +7,15 @@ const input = fs
   .map((el) => parseInt(el));
 const [a, b, c, d, e, f] = [input[0], input[1], input[2], input[3], input[4], input[5]];
 
-let x = (b * f - c * e) / (b * d - a * e);
-let y = (c - a * x) / b; // y = (f - d * x)/e
+/* 
+    adx + bdy = cd
+  - adx + aey = af
+  ==================
+        (bd-ae)y = (cd - af)
+        y = (cd - af) / (bd - ae)
+*/
 
-console.log(x, y);
+let y = (c * d - a * f) / (b * d - a * e);
+let x = (c - b * y) / a || (f - e * y) / d;
+
+console.log(`${x} ${y}`);

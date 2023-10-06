945ec3268b7fb429093bfe568e708b4f7d460057
Kimbangul <highcolor_12@naver.com>
2023-10-06 14:46:15 +0900

    231006

--- baekjoon/grade12/1018.js
+++ baekjoon/grade12/1018.js
@@ -1,12 +1,31 @@
 // /dev/stdin
 const fs = require('fs');
 let input = fs.readFileSync('./input.txt').toString().split('\n');
-let number = input
+const [y, x] = input
   .shift()
   .split(' ')
   .map((el) => parseInt(el));
-console.log(number);
 input = input.map((el) => el.trim().split(''));
+let answer = [];
 
-console.log(input);
-input.w;
+// console.log(number, input);
+const maxX = x - 8;
+const maxY = y - 8;
+
+for (let j = 0; j <= maxY; j++) {
+  // 세로
+  for (let i = 0; i <= maxX; i++) {
+    // 가로
+
+    for (let k = 0; k < 8; k++) {
+      // 1줄씩
+      console.log(j, i, k);
+      console.log(input[j][i + k]);
+    }
+  }
+}
+
+// if (number[0] % 2 === 0) {
+//   // 짝수
+
+// }

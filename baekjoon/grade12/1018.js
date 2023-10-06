adadff0c929ff7bf5d36ab65f3c6965cb694a851
Kimbangul <highcolor_12@naver.com>
2023-10-06 16:00:40 +0900

    231006

--- baekjoon/grade12/1018.js
+++ baekjoon/grade12/1018.js
@@ -7,25 +7,36 @@ const [y, x] = input
   .map((el) => parseInt(el));
 input = input.map((el) => el.trim().split(''));
 let answer = [];
-
-// console.log(number, input);
 const maxX = x - 8;
 const maxY = y - 8;
 
-for (let j = 0; j <= maxY; j++) {
-  // 세로
-  for (let i = 0; i <= maxX; i++) {
-    // 가로
+function makeBoard(xPos, yPos) {
+  const startWord = input[yPos][xPos];
+  const line = startWord === 'W' ? ['WBWBWBWB', 'BWBWBWBW'] : ['BWBWBWBW', 'WBWBWBWB'];
+  let count = 0;
 
-    for (let k = 0; k < 8; k++) {
-      // 1줄씩
-      console.log(j, i, k);
-      console.log(input[j][i + k]);
+  for (let i = 0; i < 8; i++) {
+    const currentLine = line[i % 2];
+    // console.log(currentLine);
+
+    for (let j = 0; j < 8; j++) {
+      if (input[yPos + i][xPos + j] !== currentLine[j]) {
+        count++;
+      }
     }
+  }
+
+  answer.push(count);
+}
+
+for (let j = 0; j <= maxY; j++) {
+  // console.log(`======${input[j][0]}=======`);
+  // 시작점 - 세로
+  for (let i = 0; i <= maxX; i++) {
+    // 시작점 - 가로
+    makeBoard(i, j);
   }
 }
 
-// if (number[0] % 2 === 0) {
-//   // 짝수
-
-// }
+console.log(answer);
+console.log(Math.min(...answer));

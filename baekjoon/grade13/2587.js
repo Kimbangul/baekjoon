18212976861d198d7262e86491f0cb6856800e82
Kimbangul <highcolor_12@naver.com>
2023-10-11 08:31:59 +0900

    231011 대표값2 버블정렬

--- baekjoon/grade13/2587.js
+++ baekjoon/grade13/2587.js
@@ -7,8 +7,24 @@ let input = fs
   .split('\n')
   .map((el) => parseInt(el));
 
-const midNum = Math.round(input.length / 2);
-const mid = input.sort((a, b) => a - b)[midNum - 1];
-const average = input.reduce((prev, cur) => prev + cur, 0) / input.length;
+// -- 1
+// const midNum = Math.round(input.length / 2);
+// const mid = input.sort((a, b) => a - b)[midNum - 1];
+// const average = input.reduce((prev, cur) => prev + cur, 0) / input.length;
 
+// console.log(`${average}\n${mid}`);
+
+const midNum = Math.round(input.length / 2);
+
+for (let i = 0; i < input.length - 1; i++) {
+  for (let j = i + 1; j < input.length; j++) {
+    if (input[i] > input[j]) {
+      let tmp = input[i];
+      input[i] = input[j];
+      input[j] = tmp;
+    }
+  }
+}
+const mid = input[midNum - 1];
+const average = input.reduce((prev, cur) => prev + cur, 0) / input.length;
 console.log(`${average}\n${mid}`);

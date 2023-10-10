ceb70c5e8577bf03ceea5310e569dad06d8c780a
Kimbangul <highcolor_12@naver.com>
2023-10-11 08:12:58 +0900

    231011

--- baekjoon/grade13/2750.js
+++ baekjoon/grade13/2750.js
@@ -1,16 +1,21 @@
 // /dev/stdin
 const fs = require('fs');
-const input = fs
+let input = fs
   .readFileSync('./input.txt')
   .toString()
   .trim()
   .split('\n')
   .map((el) => parseInt(el));
 
-const sortedArr = Array.from(set).sort((a, b) => a - b);
+  -- 1
+input = input.slice(1, input.length);
 let result = '';
+const sortedArr = input.sort((a, b) => a - b);
+
 sortedArr.forEach((el) => {
   result = result + '\n' + el.toString();
 });
 
 console.log(result.trim());
+
+console.log(input);

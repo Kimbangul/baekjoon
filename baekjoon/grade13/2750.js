61c6171e31aa5b8bf32937942b4b62897862c75b
Kimbangul <highcolor_12@naver.com>
2023-10-11 08:14:17 +0900

    231011 수 정렬하기

--- baekjoon/grade13/2750.js
+++ baekjoon/grade13/2750.js
@@ -7,15 +7,38 @@ let input = fs
   .split('\n')
   .map((el) => parseInt(el));
 
-  -- 1
-input = input.slice(1, input.length);
-let result = '';
-const sortedArr = input.sort((a, b) => a - b);
+//   -- 1
+// input = input.slice(1, input.length);
+// let result = '';
+// const sortedArr = input.sort((a, b) => a - b);
 
-sortedArr.forEach((el) => {
+// sortedArr.forEach((el) => {
+//   result = result + '\n' + el.toString();
+// });
+
+// console.log(result.trim());
+
+// -- 2
+input = input.slice(1, input.length);
+
+for (let i = 0; i < input.length - 1; i++) {
+  for (let j = i + 1; j < input.length; j++) {
+    let tmp;
+
+    if (input[i] > input[j]) {
+      tmp = input[j];
+      input[j] = input[i];
+      input[i] = tmp;
+      // console.log(input);
+    }
+  }
+}
+
+let result = '';
+input.forEach((el) => {
   result = result + '\n' + el.toString();
 });
 
 console.log(result.trim());
 
-console.log(input);
+// console.log(input);

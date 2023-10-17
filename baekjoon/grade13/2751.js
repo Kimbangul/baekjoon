29df53a09b6f627aa7262cabc4e4124cc2be8904
Kimbangul <highcolor_12@naver.com>
2023-10-17 10:34:56 +0900

    231017 수 정렬하기 2 병합정렬

--- baekjoon/grade13/2751.js
+++ baekjoon/grade13/2751.js
@@ -8,31 +8,29 @@ let input = fs
   .map((el) => parseInt(el));
 
 let result = '';
-// const list = input.slice(1, input.length).sort((a, b) => a - b); -- 1
-
-// list.forEach((el)  => {
-//   result+=el+'\n'
-// });
-// console.log(result.trim());
-
 const merge = (left, right) => {
   const result = [];
-  console.log(left, right);
   while (left.length !== 0 && right.length !== 0) {
     left[0] <= right[0] ? result.push(left.shift()) : result.push(right.shift());
-    console.log(left, right);
   }
 
   return [...result, ...left, ...right];
 };
 
 const mergeSort = (arr) => {
-  if (arr.length === 1) return Array;
+  if (arr.length === 1) return arr;
 
   const mid = Math.floor(arr.length / 2);
   const left = arr.slice(0, mid);
   const right = arr.slice(mid);
-  console.log(`mergeSort : ${left} ${right}`);
 
   return merge(mergeSort(left), mergeSort(right));
 };
+
+const resultArr = mergeSort(input.slice(1, input.length));
+
+resultArr.forEach((el) => {
+  result += el + '\n';
+});
+
+console.log(result.trim());

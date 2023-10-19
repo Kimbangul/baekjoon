cc086de3c9592eb6d7e68c10f2c9839b2a0019f6
Kimbangul <highcolor_12@naver.com>
2023-10-19 13:05:56 +0900

    231019

--- baekjoon/grade13/10989.ts
+++ baekjoon/grade13/10989.ts
@@ -2,12 +2,40 @@
 const fs = require('fs');
 var input = fs.readFileSync('./input.txt').toString().split('\n').map((el:string) => parseInt(el));
 
-let result : string = '';
-const list : number[] = input.slice(1);
+const n = input[0];
+input.splice(0, 1);
+console.log(input)
+const max = Math.max(...input);
+let newArr = Array(max).fill(0);
+console.log(newArr);
+let count = 0;
 
-const sortedList = list.sort((a,b)=>a-b);
-sortedList.forEach((el) => {
-  result+=el+'\n';
-});
 
-console.log(result.trim());
+for (let i = 0; i <= max; i++) {
+  for (let j = 0; j < input.length; j++) {
+    if (input[j] === i) {
+      count++;
+    }
+  }
+  newArr[i] = count;
+  console.log(newArr);
+  if (count !== 0) {
+    for (let k = 0; k < newArr[i]; k++) {
+      console.log(i);
+    }
+  }
+  count = 0;
+}
+
+// let result : string = '';
+// const list : number[] = input.slice(1);
+// let result : string = '';
+// const list : number[] = input.slice(1);
+
+// const sortedList = list.sort((a,b)=>a-b);
+// sortedList.forEach((el) => {
+//   result+=el+'\n';
+// });
+
+// console.log(result.trim());
+

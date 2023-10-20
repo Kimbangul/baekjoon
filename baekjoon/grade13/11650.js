06612b552d73f90230983df99542b3249ea091c3
Kimbangul <highcolor_12@naver.com>
2023-10-20 09:41:24 +0900

    231020

---
+++ baekjoon/grade13/11650.js
@@ -0,0 +1,9 @@
+// /dev/stdin
+const fs = require('fs');
+var input = fs
+  .readFileSync('./input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.split(' ').map((el) => parseInt(el)));
+console.log(input);

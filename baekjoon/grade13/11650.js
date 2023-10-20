2d647b7138d4927ecb679ca1b4683534d43a91ee
Kimbangul <highcolor_12@naver.com>
2023-10-20 09:52:11 +0900

    231020

--- baekjoon/grade13/11650.js
+++ baekjoon/grade13/11650.js
@@ -6,4 +6,14 @@ var input = fs
   .trim()
   .split('\n')
   .map((el) => el.split(' ').map((el) => parseInt(el)));
+
+input.shift();
 console.log(input);
+
+input.sort((a, b) => {
+  if (a[0] === b[0]) {
+    return a[1] - b[1];
+  }
+  return a[0] - b[0];
+});
+console.log(input.join('\n').replaceAll(',', ' ').trim());

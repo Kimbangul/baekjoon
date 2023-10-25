7cd5e95a44b02528dd9c4312d4d339e52d51c364
Kimbangul <highcolor_12@naver.com>
2023-10-25 14:09:01 +0900

    231025 좌표 정렬하기 2

---
+++ baekjoon/grade13/11651.js
@@ -0,0 +1,19 @@
+// /dev/stdin
+const fs = require('fs');
+var input = fs
+  .readFileSync('./input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.split(' ').map((el) => parseInt(el)));
+
+input.shift();
+
+input.sort((a, b) => {
+  if (a[1] === b[1]) {
+    return a[0] - b[0];
+  }
+  return a[1] - b[1];
+});
+
+console.log(input.join('\n').replaceAll(',', ' ').trim());

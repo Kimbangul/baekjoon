0b9455a38e5916cd62870e22fc8974956dbf6f5b
Kimbangul <highcolor_12@naver.com>
2023-10-31 08:59:50 +0900

    231031

--- baekjoon/grade14/14425.js
+++ baekjoon/grade14/14425.js
@@ -1,14 +1,20 @@
 // /dev/stdin
 const fs = require('fs');
 let input = fs.readFileSync('./input.txt').toString().split('\n');
+let result = 0;
 
 const arrNum = input
   .shift()
   .split(' ')
   .map((el) => parseInt(el));
-console.log(arrNum);
 
 const compareArr = input.slice(0, arrNum[0]);
 const targetArr = input.slice(arrNum[0]);
 
-console.log(compareArr, targetArr);
+for (let i = 0; i < targetArr.length; i++) {
+  for (let j = 0; j < compareArr.length; j++) {
+    if (targetArr[i].includes(compareArr[j])) result++;
+  }
+}
+
+console.log(result);

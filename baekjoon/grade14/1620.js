0a3eee2403643328f7bab45d0808ca265e84aaa7
Kimbangul <highcolor_12@naver.com>
2023-11-01 13:14:12 +0900

    231101

--- baekjoon/grade14/1620.js
+++ baekjoon/grade14/1620.js
@@ -1,27 +1,31 @@
 // /dev/stdin
 const fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().split('\n');
+let input = fs
+  .readFileSync('./input.txt')
+  .toString()
+  .split('\n')
+  .map((el) => el.trim());
 const [n, m] = input
   .shift()
   .split(' ')
   .map((el) => parseInt(el));
+let answer = [];
 
 const dictionary = input.slice(0, n);
-
 const questionArr = input.slice(n);
-const dictionaryMap = new Map();
-const dictionaryReverseMap = new Map();
+const dictionaryObj = {};
 
-dictionary.forEach((val, idx) => {
-  let value = val.trim();
-  dictionaryMap.set(idx + 1, value);
-  dictionaryReverseMap.set(value, idx + 1);
+console.log(questionArr);
+dictionary.forEach((el, idx) => {
+  dictionaryObj[el] = idx + 1;
+  dictionaryObj[idx + 1] = el;
 });
+
+console.log(dictionaryObj);
 
 questionArr.forEach((el) => {
-  if (isNaN(el)) {
-    console.log(dictionaryReverseMap.get(el.trim()));
-  } else {
-    console.log(dictionaryMap.get(parseInt(el)));
-  }
+  answer.push(dictionaryObj[el]);
 });
+
+console.log(answer.join('\n'));
+// console.log(input);

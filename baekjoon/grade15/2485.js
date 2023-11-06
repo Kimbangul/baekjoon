bb871e573cac684e2860278d2d3de9e18dfe1412
Kimbangul <highcolor_12@naver.com>
2023-11-07 08:09:22 +0900

    231107 가로수

--- baekjoon/grade15/2485.js
+++ baekjoon/grade15/2485.js
@@ -1,13 +1,18 @@
 const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const fs = require('fs');
-const input = fs
-  .readFileSync(path)
-  .toString()
-  .split('\n')
-  .map((el) => Number(el));
-input.shift();
+let [N, ...input] = fs.readFileSync(path).toString().trim().split('\n');
+
+input = input.map(Number);
+
+let dist = [];
+input.reduce((prev, cur) => {
+  dist.push(cur - prev);
+  return cur;
+});
+dist.sort((a, b) => a - b);
 
 const gcd = (a, b) => {
+  if (a === 0) return b;
   let tmp;
   while (b !== 0) {
     tmp = a % b;
@@ -16,13 +21,17 @@ const gcd = (a, b) => {
   }
   return a;
 };
-let answer = input[0];
+let answer = gcd(dist.pop(), dist.pop());
 
-input.forEach((el, idx) => {
-  answer = gcd(answer, el);
+while (dist.length > 0) {
+  answer = gcd(answer, dist.pop());
+}
+
+let cnt = 0;
+
+input.reduce((acc, cur) => {
+  cnt += Math.floor((cur - 1 - acc) / answer);
+  return cur;
 });
 
-console.log(answer);
-
-const distance = Math.max(...input) - Math.min(...input);
-console.log(distance / answer - (input.length - 1));
+console.log(cnt);

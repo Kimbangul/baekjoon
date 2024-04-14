128e317c1e0e73566b5bc07c44ca470ec88c74f2
Kimbangul <highcolor_12@naver.com>
2024-04-15 08:53:57 +0900

    240415

--- baekjoon/grade15/17103.js
+++ baekjoon/grade15/17103.js
@@ -5,21 +5,68 @@
 */
 const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const fs = require('fs');
-const input = fs.readFileSync(path).toString();
-
-console.log(input);
+const input = fs
+  .readFileSync(path)
+  .toString()
+  .split('\n')
+  .map((el) => parseInt(el));
 
 // 소수 판별 함수
 const isPrime = (n) => {
   if (n < 2) return false;
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) return;
-    return true;
+  for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
+    if (n % i === 0) return false;
   }
+  return true;
 };
 
+for (let idx = 1; idx < input.length; idx++) {
+  let cnt = 0;
+  for (let i = 2; i <= input[idx] / 2; i++) {
+    // 두 소수의 순서만 다른 것은 같은 파티션이므로, input[idx]의 절반까지만 구한다.
+    if (isPrime(i) && isPrime(input[idx] - i)) {
+      cnt++;
+    }
+  }
+
+  console.log(cnt);
+}
+
 /**
- *
- *
- *
+6 -> 1 + 5, 2+4, 3+3,4+2,5+1 -> 3개
+8 1+7 2.6 3,5 4,4 5,3 6,2 7,1 -> 4개
+10
+12
+100
+ **/
+
+/** 시간초과
+  const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const fs = require('fs');
+const input = fs
+  .readFileSync(path)
+  .toString()
+  .split('\n')
+  .map((el) => parseInt(el));
+
+// 소수 판별 함수
+const isPrime = (n) => {
+  if (n < 2) return false;
+  for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
+
+for (let idx = 1; idx < input.length; idx++) {
+  let cnt = 0;
+  for (let i = 2; i <= input[idx] / 2; i++) {
+    // 두 소수의 순서만 다른 것은 같은 파티션이므로, input[idx]의 절반까지만 구한다.
+    if (isPrime(i) && isPrime(input[idx] - i)) {
+      cnt++;
+    }
+  }
+
+  console.log(cnt);
+}
  */
---
+++ baekjoon/grade15/test.js
@@ -0,0 +1,41 @@
+const isPrime = (n) => {
+  if (n < 2) return false;
+  console.log(`let i = 2; i <= ${parseInt(Math.sqrt(n))}; i++`);
+  for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
+
+test('소수 판별 함수', () => {
+  expect(isPrime(3)).toBe(true);
+});
+
+/**
+ * -  숫자나 문자와 같은 객체가 아닌 기본형(primitive) 값을 비교할 때 사용
+ * test("테스트 설명", () => {
+  expect("검증 대상").toXxx("기대 결과");
+});
+
+- 객체 검증 시
+test("return a user object", () => {
+  expect(getUser(1)).toEqual({
+    id: 1,
+    email: `user1@test.com`,
+  });
+});
+
+- 예외 발생여부 테스트
+test("throw when id is non negative", () => {
+  expect(() => getUser(-1)).toThrow();
+  expect(() => getUser(-1)).toThrow("Invalid ID");
+});
+
+- 배열 테스트
+test("array", () => {
+  const colors = ["Red", "Yellow", "Blue"];
+  expect(colors).toHaveLength(3); - 배열의 길이 체크
+  expect(colors).toContain("Yellow"); - 특정 원소가 배열에 들어있는지
+  expect(colors).not.toContain("Green"); - 함수가 불만족하는지
+});
+ */

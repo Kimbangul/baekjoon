ea29be048ec99fffb6a921f55b08a74c68b015ea
yerime <yerime@hivelab.co.kr>
2025-08-21 14:25:44 +0900

    250821

--- baekjoon/grade14/14425.js
+++ baekjoon/grade14/14425.js
@@ -1,6 +1,17 @@
+/*
+총 N개의 문자열로 이루어진 집합 S가 주어진다.
+입력으로 주어지는 M개의 문자열 중에서 집합 S에 포함되어 있는 것이 총 몇 개인지 구하는 프로그램을 작성하시오.
+
+첫째 줄에 문자열의 개수 N과 M (1 ≤ N ≤ 10,000, 1 ≤ M ≤ 10,000)이 주어진다.
+다음 N개의 줄에는 집합 S에 포함되어 있는 문자열들이 주어진다.
+다음 M개의 줄에는 검사해야 하는 문자열들이 주어진다.
+입력으로 주어지는 문자열은 알파벳 소문자로만 이루어져 있으며, 길이는 500을 넘지 않는다. 집합 S에 같은 문자열이 여러 번 주어지는 경우는 없다.
+*/
+
 // /dev/stdin
 const fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
 let result = 0;
 
 const arrNum = input
@@ -8,13 +19,11 @@ const arrNum = input
   .split(' ')
   .map((el) => parseInt(el));
 
-const compareArr = input.slice(0, arrNum[0]);
+const compareSet = new Set(input.slice(0, arrNum[0]));
 const targetArr = input.slice(arrNum[0]);
-let targetObj = {};
 
-compareArr.forEach((el) => (targetObj[el.trim()] = true));
-targetArr.forEach((el) => {
-  if (targetObj[el.trim()]) result++;
+targetArr.forEach((string) => {
+  if (compareSet.has(string)) result++;
 });
 
 console.log(result);

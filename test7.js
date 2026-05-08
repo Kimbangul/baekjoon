e08e5e3c7f46521f655695aad023eaa63e49ee9d
yerime <highcolor_12@naver.com>
2026-05-08 13:52:05 +0900

    260508

--- test7.js
+++ test7.js
@@ -254,53 +254,68 @@ function solution4(genres, plays) {
   return answer;
 }
 
-console.log(solution4(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]));
-console.log(solution4(
-  ["pop", "pop", "pop"],
-  [100, 200, 300]
-)); // [2, 1]
+/* 
+신입사원 무지는 게시판 불량 이용자를 신고하고 처리 결과를 메일로 발송하는 시스템을 개발하려 합니다. 무지가 개발하려는 시스템은 다음과 같습니다.
+각 유저는 한 번에 한 명의 유저를 신고할 수 있습니다.
+신고 횟수에 제한은 없습니다. 서로 다른 유저를 계속해서 신고할 수 있습니다.
+한 유저를 여러 번 신고할 수도 있지만, 동일한 유저에 대한 신고 횟수는 1회로 처리됩니다.
+k번 이상 신고된 유저는 게시판 이용이 정지되며, 해당 유저를 신고한 모든 유저에게 정지 사실을 메일로 발송합니다.
+유저가 신고한 모든 내용을 취합하여 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송합니다.
+다음은 전체 유저 목록이 ["muzi", "frodo", "apeach", "neo"]이고, k = 2(즉, 2번 이상 신고당하면 이용 정지)인 경우의 예시입니다.
 
-console.log(solution4(
-  ["a", "b", "c"],
-  [100, 200, 300]
-)); // [2, 1, 0]
+유저 ID	유저가 신고한 ID	설명
+"muzi"	"frodo"	"muzi"가 "frodo"를 신고했습니다.
+"apeach"	"frodo"	"apeach"가 "frodo"를 신고했습니다.
+"frodo"	"neo"	"frodo"가 "neo"를 신고했습니다.
+"muzi"	"neo"	"muzi"가 "neo"를 신고했습니다.
+"apeach"	"muzi"	"apeach"가 "muzi"를 신고했습니다.
 
-console.log(solution4(
-  ["rock", "rock", "rock"],
-  [300, 300, 300]
-)); // [0, 1]
+각 유저별로 신고당한 횟수는 다음과 같습니다.
 
-console.log(solution4(
-  ["a", "b", "a", "b", "c"],
-  [100, 200, 300, 400, 500]
-)); // [4, 3, 1, 2, 0]
+유저 ID	신고당한 횟수
+"muzi"	1
+"frodo"	2
+"apeach"	0
+"neo"	2
+위 예시에서는 2번 이상 신고당한 "frodo"와 "neo"의 게시판 이용이 정지됩니다. 
+이때, 각 유저별로 신고한 아이디와 정지된 아이디를 정리하면 다음과 같습니다.
+유저 ID	유저가 신고한 ID	정지된 ID
+"muzi"	["frodo", "neo"]	["frodo", "neo"]
+"frodo"	["neo"]	["neo"]
+"apeach"	["muzi", "frodo"]	["frodo"]
+"neo"	없음	없음
+따라서 "muzi"는 처리 결과 메일을 2회, "frodo"와 "apeach"는 각각 처리 결과 메일을 1회 받게 됩니다.
+이용자의 ID가 담긴 문자열 배열 id_list, 각 이용자가 신고한 이용자의 ID 정보가 담긴 문자열 배열 report, 정지 기준이 되는 신고 횟수 k가 매개변수로 주어질 때, 
+각 유저별로 처리 결과 메일을 받은 횟수를 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+*/
 
-console.log(solution4(
-  ["hiphop", "hiphop", "hiphop", "pop"],
-  [100, 200, 300, 400]
-)); // [2, 1, 3]
+function solution5(id_list, report, k) {
+  const answer = new Array(id_list.length).fill(0);
+  const reportCnt = new Object();
+  const completeCnt = new Object();
 
-console.log(solution4(
-  ["a", "b", "c", "a", "b", "c"],
-  [10, 20, 30, 40, 50, 60]
-)); // [5, 2, 4, 1, 3, 0]
+  id_list.forEach(( id ) => {
+    reportCnt[id] = 0;
+  });
 
-console.log(solution4(
-  ["jazz", "jazz", "jazz", "jazz"],
-  [500, 500, 300, 300]
-)); // [0, 1]
+  report = [...new Set(report)];
 
-console.log(solution4(
-  ["solo"],
-  [999]
-)); // [0]
+  report.forEach(( reportItem ) => {
+    const item = reportItem.split(' ');
+    if ( !completeCnt[item[0]] ) completeCnt[item[0]] = new Array();
+    completeCnt[item[0]].push(item[1]);
+    reportCnt[item[1]]++;
+  });
 
-console.log(solution4(
-  ["a", "b", "c", "a", "b"],
-  [100, 200, 300, 400, 50]
-)); // [3, 0, 2, 1, 4]
+  id_list.forEach(( id, idx ) => {
+    if ( !completeCnt[id] ) return;
+    completeCnt[id].forEach(( cntId ) => {
+      if ( reportCnt[cntId] >= k ) answer[idx]++;
+    });
+  });
 
-console.log(solution4(
-  ["classic", "pop", "classic", "classic", "pop"],
-  [500, 600, 150, 800, 600]
-)); // [3, 0, 1, 4]
+  return answer;
+}
+
+console.log(solution5(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)); // [2,1,1,0]
+console.log(solution5(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3)); // [0,0]

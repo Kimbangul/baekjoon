ebd33e48b1124ed323f0bb5db3b6e13c9c60c689
yerime <highcolor_12@naver.com>
2026-05-22 16:22:44 +0900

    260522

--- test8.js
+++ test8.js
@@ -144,7 +144,105 @@ function solution2(list, searchList) {
   return result;
 }
 
-solution2([5, 3, 8, 4, 2, 1, 7, 10], [1, 2, 5, 6]);
+// solution2([5, 3, 8, 4, 2, 1, 7, 10], [1, 2, 5, 6]);
 
-console.log(solution2([5, 3, 8, 4, 2, 1, 7, 10], [1, 2, 5, 6]));
-console.log(solution2([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]));
+// console.log(solution2([5, 3, 8, 4, 2, 1, 7, 10], [1, 2, 5, 6]));
+// console.log(solution2([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]));
+
+
+/* 
+△△ 게임대회가 개최되었습니다. 이 대회는 N명이 참가하고, 토너먼트 형식으로 진행됩니다. 
+N명의 참가자는 각각 1부터 N번을 차례대로 배정받습니다. 그리고, 1번↔2번, 3번↔4번, ... , 
+N-1번↔N번의 참가자끼리 게임을 진행합니다. 각 게임에서 이긴 사람은 다음 라운드에 진출할 수 있습니다. 
+이때, 다음 라운드에 진출할 참가자의 번호는 다시 1번부터 N/2번을 차례대로 배정받습니다. 
+만약 1번↔2번 끼리 겨루는 게임에서 2번이 승리했다면 다음 라운드에서 1번을 부여받고, 
+3번↔4번에서 겨루는 게임에서 3번이 승리했다면 다음 라운드에서 2번을 부여받게 됩니다. 
+게임은 최종 한 명이 남을 때까지 진행됩니다.
+
+이때, 처음 라운드에서 A번을 가진 참가자는 경쟁자로 생각하는 B번 참가자와 몇 번째 라운드에서 만나는지 궁금해졌습니다. 게임 참가자 수 N, 참가자 번호 A, 경쟁자 번호 B가 함수 solution의 매개변수로 주어질 때, 처음 라운드에서 A번을 가진 참가자는 경쟁자로 생각하는 B번 참가자와 몇 번째 라운드에서 만나는지 return 하는 solution 함수를 완성해 주세요. 단, A번 참가자와 B번 참가자는 서로 붙게 되기 전까지 항상 이긴다고 가정합니다.
+
+제한사항
+N : 2^1 이상 2^20 이하인 자연수 (2의 지수 승으로 주어지므로 부전승은 발생하지 않습니다.)
+A, B : N 이하인 자연수 (단, A ≠ B 입니다.)
+
+N	A	B	answer
+8	4	7	3
+
+출력 예 #1
+첫 번째 라운드에서 4번 참가자는 3번 참가자와 붙게 되고, 7번 참가자는 8번 참가자와 붙게 됩니다. 
+항상 이긴다고 가정했으므로 4번 참가자는 다음 라운드에서 2번이 되고, 7번 참가자는 4번이 됩니다. 
+두 번째 라운드에서 2번은 1번과 붙게 되고, 4번은 3번과 붙게 됩니다. 항상 이긴다고 가정했으므로
+2번은 다음 라운드에서 1번이 되고, 4번은 2번이 됩니다. 
+세 번째 라운드에서 1번과 2번으로 두 참가자가 붙게 되므로 3을 return 하면 됩니다.
+*/
+
+// class Node {
+//   constructor(key) {
+//     this.val = key;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// class BST {
+//   constructor() {
+//     this.root = null;
+//   }
+
+//   insert(key) {
+//     if ( !this.root ) this.root = new Node(key);
+//     else {
+//       let curr = this.root;
+      
+//       while (true) {
+//         if ( !curr.left ) {
+//           curr.left = new Node(key);
+//           break;
+//         } else {
+//           curr = curr.left;
+//         }
+//       }
+//     }
+//   }
+// }
+
+function solution3(n,a,b) {
+  // 맨 처음은 무조건 1번은 만남
+  let answer = 1;
+  let leafNode = new Array(n).fill(1).map((num, idx) => num + idx);
+
+  // 종료조건 1:  더 이상 상위 노드가 없을 떄
+  outer: while ( leafNode.length > 1 ) {
+    const newNode = [];
+    
+    // 2개씩 짝지어서 상위로 올릴 노드를 검사함
+    for ( let i = 0; i < leafNode.length; i = i + 2 ) {
+      let toPushNode = 0;
+      const left = leafNode[i];
+      const right = leafNode[i + 1];
+      
+      for (const node of [left, right]) {
+        if ( [a, b].includes(node) ) {
+          if ( toPushNode !== 0 ) {
+            // 종료조건 2: left와 right가 a, b 일 때
+            // 즉 현재 이미 0이 아닌 노드를 상위로 올리려는 상태에서, 
+            // right까지 조건에 걸리면 a, b 가 맞짱뜨는 것
+            break outer;
+          }
+          
+          toPushNode = node;
+        }
+      };
+
+      // 올라가는 노드는 a와 b가 아니면 0으로 올라감.
+      newNode.push(toPushNode);
+    }
+
+    leafNode = newNode;
+    answer++;
+  }
+
+  return answer;
+}
+
+console.log(solution3(8, 4, 7)); 

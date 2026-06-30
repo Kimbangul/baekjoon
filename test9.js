2ae9ea265cab0a8bfb91df57b396adaf4d5be6ce
yerime <highcolor_12@naver.com>
2026-06-30 09:58:57 +0900

    260630

--- test9.js
+++ test9.js
@@ -145,3 +145,41 @@ function solution3(n, words) {
 console.log(solution3(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])); // [3,3]
 console.log(solution3(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"])); // [0,0]
 console.log(solution3(2, ["hello", "one", "even", "never", "now", "world", "draw"])); // [1,3]
+
+/* 
+전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+구조대 : 119
+박준영 : 97 674 223
+지영석 : 11 9552 4421
+전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 
+어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+
+phone_book의 길이는 1 이상 1,000,000 이하입니다.
+각 전화번호의 길이는 1 이상 20 이하입니다.
+같은 전화번호가 중복해서 들어있지 않습니다.
+
+phone_book	return
+["119", "97674223", "1195524421"]	false
+["123","456","789"]	true
+["12","123","1235","567","88"]	false
+*/
+
+function solution4(phone_book) {
+    const book = new Set(phone_book);
+
+    for ( let i = 0; i < phone_book.length; i++ ) {
+      let word = '';
+      for ( const j of phone_book[i] ) {
+        word += j;
+        if ( book.has(word) && word !== phone_book[i] ) return false;
+      }
+    }
+
+    return true;
+}
+
+console.log(solution4(["119", "97674223", "1195524421"]	)); // false
+console.log(solution4(["123","456","789"]	)); // true
+console.log(solution4(["12","123","1235","567","88"]	)); // false
